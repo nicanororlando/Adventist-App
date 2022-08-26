3ffca55f3af28b51975c46bb5a8d54386eb98454
nicanor <nicanororlando@gmail.com>
2022-08-26 15:02:09 -0300

    Agrego datos de la api

--- beliefs/app/pages/Beliefs/beliefs.component.js
+++ beliefs/app/pages/Beliefs/beliefs.component.js
@@ -4,17 +4,17 @@
 angular.module("beliefs").component("beliefs", {
   templateUrl: "./pages/beliefs/beliefs.template.html",
   controller: function BeliefsController($http) {
+    this.welcome = "Our beliefs";
     var self = this;
 
+    // http://localhost:8080/api/beliefs
     $http.get("http://localhost:8080/api/beliefs").then(
       (res) => {
-        self.beliefs = res.data;
+        this.beliefs = res.data;
       },
       (res) => {
-        self.beliefs = [{ name: "Error!" + res.status }];
+        this.beliefs = [{ name: "Error!" + res.status }];
       }
     );
-
-    this.welcome = "Our beliefs";
   },
 });
