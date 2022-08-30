2fe3a9cbc287ef07726ac60026ec831219dea168
nicanor <nicanororlando@gmail.com>
2022-08-29 22:19:55 -0300

    More details and verses data updated.

--- beliefs/app/components/BeliefCard/scripts.js
+++ beliefs/app/components/BeliefCard/scripts.js
@@ -1,6 +1,30 @@
+var isCard;
+
 angular.module("beliefCard", []).component("beliefCard", {
   templateUrl: "./components/BeliefCard/index.html",
+  controller: function BeliefCardController($document, $window) {
+    // console.log(this.verses);
+
+    // this.verses = this.belief.description;
+    // console.log(this.verses);
+
+    if (
+      $document[0].documentElement.clientWidth < 576 ||
+      $document[0].documentElement.clientWidth > 1200
+    ) {
+      this.isCard = true;
+    } else isCard = false;
+    $window.onresize = () => {
+      if (
+        $document[0].documentElement.clientWidth < 576 ||
+        $document[0].documentElement.clientWidth > 1200
+      ) {
+        this.isCard = true;
+      } else isCard = false;
+    };
+  },
   bindings: {
     belief: "=",
+    verses: "=",
   },
 });
--- beliefs/app/pages/Beliefs/beliefs.component.js
+++ beliefs/app/pages/Beliefs/beliefs.component.js
@@ -5,12 +5,26 @@ angular.module("beliefs").component("beliefs", {
   templateUrl: "./pages/beliefs/beliefs.template.html",
   controller: function BeliefsController($http) {
     this.welcome = "Our beliefs";
-    var self = this;
 
-    // http://localhost:8080/api/beliefs
     $http.get("http://localhost:8080/api/beliefs").then(
       (res) => {
         this.beliefs = res.data;
+
+        let allVers = [];
+
+        for (let i = 0; i < this.beliefs.length; i++) {
+          let books = [];
+          books.push(_.last(this.beliefs[i].description));
+
+          let obj = {};
+          obj.id = i;
+          obj.books = _.split(books, "; ");
+
+          allVers.push(obj);
+        }
+
+        this.verses = allVers;
+        console.log(this.verses);
       },
       (res) => {
         this.beliefs = [{ name: "Error!" + res.status }];
