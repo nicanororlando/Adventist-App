c50f768dcecce6f492c4fce6cc6acd90dbd2f7db
nicanor <nicanororlando@gmail.com>
2022-09-01 13:26:26 -0300

    complete belief working

--- beliefs/app/pages/CompleteBelief/belief.component.js
+++ beliefs/app/pages/CompleteBelief/belief.component.js
@@ -7,26 +7,16 @@ angular.module("completeBelief").component("completeBelief", {
     "$routeParams",
     "$http",
     function CompleteBeliefsController($routeParams, $http) {
-      var self = this;
-
-      this.welcome = "Belief";
-
-      let url =
-        //  "http://localhost:8080/api/complete-beliefs/the-holy-scriptures";
-        // let url2 =
-        "http://localhost:8080/api/beliefs/" + $routeParams.beliefId;
+      let url = "http://localhost:8080/api/beliefs/" + $routeParams.beliefId;
 
       $http.get(url).then(
         (res) => {
           this.belief = res.data;
         },
         (res) => {
-          s;
           this.belief = [{ name: "Error!" + res.status }];
         }
       );
-
-      // this.beliefs = Belief.query();
     },
   ],
 });
