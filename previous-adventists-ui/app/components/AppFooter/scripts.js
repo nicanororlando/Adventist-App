7a1b1089ffb3e0abecd439ffb889e60cf3d94f97
nicanororlando <nicanororlando@gmail.com>
2023-03-02 12:26:46 -0300

    bootstrap ui deleted from main branch

--- previous-adventists-ui/app/components/AppFooter/scripts.js
+++
@@ -1,3 +0,0 @@
-angular.module("appFooter", []).component("appFooter", {
-  templateUrl: "./components/AppFooter/index.html",
-});
--- previous-adventists-ui/app/pages/CompleteBelief/belief.module.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-
-angular.module("completeBelief", ["ngRoute", "core.belief"]);
